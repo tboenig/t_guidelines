936c47ca2f073667d0b69e2eb1948e4d2a3c723e
tboenig <matthiasboenig@gmx.net>
2023-07-10 16:53:59 +0000

    Deploying to gh-pages from @ tboenig/t_guidelines@31c7dcd24eee372e2577f8bc0427d3bf800365d3 🚀

--- html/js/lunr-client.js
+++ html/js/lunr-client.js
@@ -1,6 +1,6 @@
 const LUNR_DATA = {"version":"2.3.9","fields":["t","b","k"],"fieldVectors":[],"invertedIndex":[],"pipeline":["stemmer"]};
 const PREVIEW_LOOKUP = {};
-const BASE_URL = 'https://tboenig.github.io/t_guidelines/html/';
+const BASE_URL = 'https://tboenig.github.io/t_guidelines/html/trans/';
 
 // Parse search results into HTML
 function parseLunrResults(results) {
