d89f67b19e4c178d255bc2fdbea4eaec8b47f10b
tboenig <matthiasboenig@gmx.net>
2023-07-10 16:40:36 +0000

    Deploying to gh-pages from @ tboenig/t_guidelines@1e4f791afc583fa7b141c4d68ad35eb2553b968c 🚀

--- html/js/lunr-client.js
+++ html/js/lunr-client.js
@@ -1,31 +1,6 @@
-let LUNR_DATA = null;
-let PREVIEW_LOOKUP = null;
-const scripts = document.getElementsByTagName("script");
-const scriptPath = scripts[scripts.length - 1].src;
-const JSON_PATH = scriptPath.substr(0, scriptPath.lastIndexOf("/") + 1);
-const BASE_URL = scriptPath.substr(
-  0,
-  JSON_PATH.lastIndexOf("/", JSON_PATH.lastIndexOf("/") - 1) + 1
-);
-
-function jsonFetch(json, filename) {
-  return new Promise(function (resolve, reject) {
-    if (json) {
-      return resolve(json);
-    }
-
-    fetch(JSON_PATH + filename)
-      .then((response) => {
-        if (!response.ok) {
-          return reject(new Error(`HTTP error, status = ${response.status}`));
-        }
-        return response.json();
-      })
-      .then((data) => {
-        return resolve(data);
-      });
-  });
-}
+const LUNR_DATA = {"version":"2.3.9","fields":["t","b","k"],"fieldVectors":[],"invertedIndex":[],"pipeline":["stemmer"]};
+const PREVIEW_LOOKUP = {};
+const BASE_URL = 'https://tboenig.github.io/t_guidelines/html/';
 
 // Parse search results into HTML
 function parseLunrResults(results) {
@@ -36,35 +11,33 @@ function parseLunrResults(results) {
     const title = item["t"];
     const preview = item["d"];
     const link = item["l"];
-    const result = `<div class="card mb-3">
-      <a class="link stretched-link link-underline link-underline-opacity-0" href="${BASE_URL + link}">
-          <h2 class="h3 title card-header text-body-emphasis">${title}</h5>
-      </a>
-      <div class="card-body">
-        <div class="card-text">
-          ${preview}
-        </div>
-      </div>
-    </div>`;
+    const result =
+      '<p><span class="h3"><a href="' +
+      BASE_URL + 
+      link +
+      '">' +
+      title +
+      "</a></span></p>" +
+      preview;
     html.push(result);
   }
   if (html.length) {
     return html.join("");
   } else {
-    return "<p>Ihre Suche ergab keine Ergebnisse</p>";
+    return "<p>Your search returned no results.</p>";
   }
 }
 
 function formatResults(results) {
   return `<article role="article">
-    <div class="modal-header">
-	        <h1>Suchergebnisse</h1>
-          <button type="button" class="btn-close" aria-label="Close" onclick="closeSearch(this);"></button>
-      </div>
-      <div class="modal-body">
-    ${parseLunrResults(results)}
-    </div>
-    </article>`;
+		<div class="modal-header">
+	        <h1>Search Results</h1>
+	        <button type="button" class="btn-close" aria-label="Close" onclick="closeSearch(this);"></button>
+	    </div>
+	    <div class="modal-body">
+		${parseLunrResults(results)}
+		</div>
+		</article>`;
 }
 
 function escapeHtml(unsafe) {
@@ -76,6 +49,26 @@ function escapeHtml(unsafe) {
     .replace(/'/g, "&#039;");
 }
 
+function showResultCount(query, total, domElementId) {
+  if (total == 0) {
+    return;
+  }
+
+  var s = "";
+  if (total > 1) {
+    s = "s";
+  }
+  var found = "<p>Found " + total + " result" + s;
+  if (query != "" && query != null) {
+    query = escapeHtml(query);
+    var forQuery = ' for <span class="result-query">' + query + "</span>";
+  } else {
+    var forQuery = "";
+  }
+  var element = document.getElementById(domElementId);
+  element.innerHTML = found + forQuery + "</p>";
+}
+
 function closeSearch(el) {
   const elements = document.getElementsByClassName("bs-main");
   elements[1].remove();
@@ -84,34 +77,22 @@ function closeSearch(el) {
 }
 
 function search(el) {
-  Promise.all([
-    jsonFetch(LUNR_DATA, "search_index.json"),
-    jsonFetch(PREVIEW_LOOKUP, "preview.json"),
-  ])
-    .then((values) => {
-      LUNR_DATA = values[0];
-      PREVIEW_LOOKUP = values[1];
-      const query = document.getElementById("search-input").value;
-      const idx = lunr.Index.load(LUNR_DATA);
-      // Write results to page
-      const results = idx.search(query);
-      const elements = document.getElementsByClassName("bs-main");
+  const query = document.getElementById("search-input").value;
+  const idx = lunr.Index.load(LUNR_DATA);
+  // Write results to page
+  const results = idx.search(query);
+  const elements = document.getElementsByClassName("bs-main");
 
-      if (elements.length < 2) {
-        const resultHtml = `<main role="main" class="container bs-main">
-          ${formatResults(results)}</main>`;
+  if (elements.length < 2) {
+    const resultHtml = `<main role="main" class="container bs-main">
+	    	${formatResults(results)}</main>`;
 
-        elements[0].insertAdjacentHTML("afterend", resultHtml);
-      } else {
-        elements[1].innerHTML = formatResults(results);
-      }
+    elements[0].insertAdjacentHTML("afterend", resultHtml);
+  } else {
+    elements[1].innerHTML = formatResults(results);
+  }
 
-      elements[0].classList.add("collapse");
-      window.scrollTo(0, 0);
-      return false;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+  elements[0].classList.add("collapse");
+  window.scrollTo(0,0);
   return false;
 }
